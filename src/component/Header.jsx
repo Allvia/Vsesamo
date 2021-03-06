a912a19b05aa76dc7750cc6a7def4e4ffc1ad6bc
Allvia <79840668+Allvia@users.noreply.github.com>
2021-03-06 22:17:39 +0300

    6032021

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import './Header.css';
+import s from './Header.module.css';
 
 
 const Header = () => {
     return (
-        <header className ='header'>
+        <header className = {s.header}>
               <img src ='https://st2.depositphotos.com/8311062/12114/i/600/depositphotos_121141058-stock-photo-vector-purple-green-tree-healing.jpg'/>
       </header> 
     )
